9c4068c1ab5805d85529f709d190141a5ee907cf
Manny <manny1449@outlook.com>
2025-03-18 22:24:08 -0400

    personal Project 10/15% done, added html and necessary files to begin project

---
+++ welcome-page/script.js
@@ -0,0 +1,4 @@
+const cards = document.getElementById("card");
+const carousel = document.getElementById("carousel-slide");
+const url =
+  "https://newsapi.org/v2/everything?q=Apple&from=2025-03-19&sortBy=popularity&apiKey=be862f3e68634812ad6c9042d1bc64ca";
